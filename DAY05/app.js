fe640651e553ebac6f342181a85d94203a6be9ba
Richard Enuoma <richardenuoma6@gmail.com>
2025-12-10 21:46:57 +0000

    Updated

--- DAY05/app.js
+++ DAY05/app.js
@@ -3,10 +3,13 @@ let btn = document.getElementById('btn');
 let result = document.getElementById('result');
 let input = document.getElementById('input');
 
+// Creating a function using farenheight as it's parameter
 function celcuisConverter(farenheight){
+    // celcuis formula 
     return 5 / 9 * (farenheight - 32);
 }
 
+// Using the event listner to get input and update result  
 btn.addEventListener('click', ()=>{
     let value = Math.round(parseFloat(input.value));
     let celcuis = celcuisConverter(value);
