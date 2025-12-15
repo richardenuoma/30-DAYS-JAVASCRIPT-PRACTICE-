fbe05a443aee35f4212f5bc6b394884cc98d1ee2
Richard Enuoma <richardenuoma6@gmail.com>
2025-12-15 22:57:44 +0000

    Updated..

---
+++ DAY07/app.js
@@ -0,0 +1,13 @@
+// Select all html elements from index.html
+let options = document.querySelectorAll('.option-button');
+let inputs = document.querySelectorAll('.input-message');
+let btn = document.querySelector('.create-btn');
+
+
+function generateStory(){
+     
+}
+
+btn.addEventListener('click', ()=>{
+    genereteStory();
+});
