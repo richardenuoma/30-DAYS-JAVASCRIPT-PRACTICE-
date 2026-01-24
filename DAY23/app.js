fec35d1f2aa2159a7158643299f05da87d162654
Richard Enuoma <richardenuoma6@gmail.com>
2026-01-24 12:00:01 +0000

    Updated

---
+++ DAY23/app.js
@@ -0,0 +1,91 @@
+// OBJECTS
+// Grouping Data 
+// Using Variables we to group arrays are slighty complex and sometimes needs to be minimized so we used arrays to replace variables now we're going to use Objects. 
+
+// example of variables storing data 
+let fruit1 = "Apple"; 
+let fruit2 = "Pear";
+let fruit3 = "Orange";
+
+// example of array storing data 
+// let fruits = ["Apple", "Pear", "Orange"];
+// This way we accessed them through their index number 0,1,2,3 
+
+// Multiple Type Arrays
+// let myCar = ["Red", 5, 3500, ["Tony", "Angela"]];
+
+// console.log("The colour of my car is: " + myCar[0])
+
+let myCar = {
+    color: "Red",
+    mileage: 3500,
+    seats: 5,
+    owner: ["Tony", "Angela"]
+}
+
+// console.log("The color of my car is: " + myCar.color);
+
+
+let myObjectName = {
+    property1Name: "property 1",
+    property2Name: "property 2",
+}
+
+//Appalachian Trail 
+let trail = {
+    name: "Appalachian Taril",
+    length: 2197,
+    location: "Appalachain Mountains",
+    highestPoint : 6643,
+    lowestPoint: 124
+}
+
+// console.log(trail);
+
+// Accessing Objects
+let mainCharacter = {
+    name: "Harry",
+    age: 11,
+    friends: ["Hermione", "Ron"]
+}
+
+// console.log(mainCharacter.name);
+// console.log(mainCharacter["name"])
+
+// Changing Property Values 
+
+let fruits = {
+    apples: 5,
+    pears: 3
+}
+// Adding a new property name and value 
+// fruits.apples = 6;
+fruits.orange = 7;
+
+// Deleting a property and it's value 
+// delete fruits.apples;
+// console.log(fruits);
+
+// for(fruit in fruits){
+//     console.log(`${fruit} ${fruits[fruit]}`);
+// }
+let fruitProperty = "pineapple";
+let fruitDigits = "100";
+
+for(fruit in fruits){
+    delete fruits[fruit];
+    fruits[fruitProperty] = Number(fruitDigits);
+}
+
+console.log(fruits);
+
+// JSON = JAVASCRIPT OBJECT NOTATION (DATA STRUCTURE)
+let order1 = {
+    number: 846344,
+    items: [
+        {name: "Screws", quantity: 40},
+        {name: "Helmets", quantity: 4},
+    ]
+}
+
+console.log(order1)
