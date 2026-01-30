f8ee186fbe2da6ec19692ef6e41bd953fb40b83f
Richard Enuoma <richardenuoma6@gmail.com>
2026-01-30 14:20:42 +0000

    Updated

--- DAY24/app.js
+++ DAY24/app.js
@@ -94,3 +94,79 @@ fruits.pop();
 // console.log(fruits);
 
 // CONCAT 
+let myList = ["Paris", "London"];
+let yourList = ["New York", "Barcelona"];
+
+let combinedList = [...myList,...yourList];
+// console.log(combinedList);
+
+let combinedList2 = myList.concat(yourList);
+// console.log(combinedList2);
+
+// ASSIGNMENT 
+/** Use the concat() method to add all of the arrays together.
+
+So the finalArray ends up as a single, flat array like this:
+
+["A","B","C","D","E","F","G","H","I","J"]; **/
+
+let array1 = ["A", "B"];
+let array2 = ["C", "D"];
+let array3 = ["E", "F"];
+let array4 = ["G", "H"];
+let array5 = ["I", "J"];
+
+let finalArray = array1.concat(array2).concat(array3).concat(array4).concat(array5);
+
+let finalArray2 = array1.concat(array2,array3,array4,array5);
+
+// console.log(finalArray);
+// console.log(finalArray2);
+
+// SPLICE 
+// array.splice(position,numberOfItemsToRemove, addItem1,....addItemX);
+
+// A mutating array method 
+let alphabet = ["A", "B", "D", "C", "E"];
+alphabet.splice(2,2 ,"C", "D");
+// console.log(alphabet);
+
+// SLICE
+// a non mutating array method
+//array.slice(startPosition, endPosition):
+const fish = ["Head", "Body", "Tail"];
+// console.log(`I will eat the fish ${fish.slice(1,2)}`);
+
+let friends = ["Rachael", "Monica", "Lisa"];
+ 
+// console.log(friends.slice(1,3));
+friends.shift();
+// console.log(friends);
+
+// Pop takes the last, shift takes the first 
+friends.unshift("Andrew", "Rebecca");
+// Unshift mutate the array
+// console.log(friends);
+
+// Reverse Fib
+function reverseFib(n){
+    let finalSequence = [0,1];
+    for(let i = 2; i < n ; i++){
+        finalSequence.push(finalSequence[i-2] + finalSequence[i-1]);
+    }
+    let reverseSequence =[];
+    for(let i = 0; i < n ; i++){
+        reverseSequence.push(finalSequence.pop());
+    }
+
+    return reverseSequence;
+}
+
+console.log(reverseFib(6))
+
+
+
+let hello = "Hello";
+hello += " world";
+console.log(hello);
+console.log(typeof null);
